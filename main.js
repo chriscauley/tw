491f0399040a30e85ec0101efdd4efdf2a757ae3
chriscauley <chris@lablackey.com>
2017-12-28 20:07:29 -0500

    draw squares onto board and only display board

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ class Square extends CanvasObject {
     this.canvas = this.newCanvas({
       width: this.scale,
       height: this.scale,
-      parent: document.getElementById("game",)
     });
     this.draw();
   }
@@ -20,7 +19,9 @@ class Square extends CanvasObject {
 }
 
 var LEVELS = [
-  [[0,0],[0,7],[7,0],[7,7]],
+  [
+    [CountDown,[[0,0],[0,7],[7,0],[7,7]]],
+  ]
 ];
 
 class Board extends CanvasObject {
@@ -36,9 +37,10 @@ class Board extends CanvasObject {
     this.squares = [];
     this.eachSquare(function(_s,x,y) { // _s is undefined;
       self.squares[x] = self.squares[x] || [];
-      self.squares[x][y] = new Square({x:x,y:y,board:self})
+      self.squares[x][y] = new Square({x:x,y:y,board:self});
     });
     this.createCanvas();
+    document.getElementById("game",).style.width = (this.width*this.scale)+"px";
     this.pieces = [ new Piece({ board: this, x:3, y:3 }) ];
     this.player = this.pieces[0];
     this.controller = new Controller({ parent: this });
@@ -51,12 +53,16 @@ class Board extends CanvasObject {
   }
   startLevel(level_number) {
     var self = this;
-    uR.forEach(LEVELS[level_number],function(level) {
-      self.pieces.push(new CountDown({
-        x: level[0],
-        y: level[1],
-        board: self,
-      }));
+    uR.forEach(LEVELS[level_number],function(feature) {
+      var feature_class = feature[0];
+      var positions = feature[1];
+      uR.forEach(positions,function(xy) {
+        self.pieces.push(new feature_class({
+          x: xy[0],
+          y: xy[1],
+          board: self,
+        }))
+      })
     });
   }
   keydown(key) {
@@ -85,21 +91,27 @@ class Board extends CanvasObject {
     this.pieces.forEach(function(p) { p.play() });
   }
   eachSquare(func) {
+    func = func.bind(this);
     for (var x=0;x<this.width;x++) {
       for (var y=0;y<this.height;y++) {
-        var square = this.squares[x] && this.squares[x][y];
+        var square = this.squares[y] && this.squares[y][x];
         func(square,x,y)
       }
     }
   }
   draw() {
-    this.eachSquare(function(square) { return square.draw(); });
-    uR.forEach(this.pieces,function(p){ p.draw()})
+    var s = this.scale;
+    this.eachSquare(function(square) { square && square.draw(); });
+    uR.forEach(this.pieces,function(p){ p.draw() })
+    this.eachSquare(function(square,x,y) {
+      square && this.canvas.ctx.drawImage(square.canvas,s*x,s*y);
+    });
   }
   createCanvas() {
     this.canvas = this.newCanvas({
-      height: this.scale*this.x,
-      width: this.scale*this.y,
+      height: this.scale*this.width,
+      width: this.scale*this.height,
+      parent: document.getElementById("game")
     });
   }
   getSquare(x,y) { return this.squares[y] && this.squares[y][x] }
