6f840153cb6a6f51fe0fc6e3a4e39c3ed8c042b2
chriscauley <chris@lablackey.com>
2018-02-06 19:32:40 -0500

    using requestAnimationFrame

--- board.js
+++ board.js
@@ -17,15 +17,15 @@ class Board extends CanvasObject {
     this.pieces = [];
     this.createCanvas();
     document.getElementById("game",).style.width = (this.W*this.scale)+"px";
+    this.tick = this.tick.bind(this);
     this.tick();
     this.dirty = true;
+    this.__tick = 0;
   }
   tick() {
-    this._f = this.f||0;
     cancelAnimationFrame(this.animation_frame);
-    if (this._f>100) { return }
-    this.draw();
-    this.animation_frame = requestAnimationFrame(this.tick.bind(this));
+    (this.__tick++)%2 && this.draw();
+    this.animation_frame = requestAnimationFrame(this.tick);
   }
   loadLevel(level_number) {
     delete this.squares;
