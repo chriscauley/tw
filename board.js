37db03b8d3cf4a1c7e3c18a75463a9443df83f29
chriscauley <chris@lablackey.com>
2018-02-06 19:33:07 -0500

    draw floor canvas separately

--- board.js
+++ board.js
@@ -78,28 +78,46 @@ class Board extends CanvasObject {
     }
   }
   draw() {
+    if (!this.game.player) { return }
     var s = this.scale;
     var offset_x = (this.game.player.x-this.W/2)*this.x_offset_mult;
     var offset_y = (this.game.player.y-this.H/2)*this.y_offset_mult;
-    this.canvas.ctx.clearRect(0,0,this.W*s,this.H*s);
-    this.canvas.ctx.translate(-offset_x,-offset_y)
+    var floor_dirty = true;
     this.eachSquare(function(square,x,y) {
-      square && square.draw();
-      square && this.canvas.ctx.drawImage(square.canvas,s*x,s*y);
+      floor_dirty = (square && square.draw()) || floor_dirty;
     });
+    if (floor_dirty) {
+      this.floor_canvas.clear();
+      this.eachSquare(function(square,x,y) {
+        square && this.floor_canvas.ctx.drawImage(square.canvas,s*x-offset_x,s*y-offset_y);
+      });
+    }
+    var dirty = floor_dirty || this.game.player.dirty;
+    !dirty && uR.forEach([this.pieces,this.player],function(group) {
+      group && uR.forEach(group,function(i) { dirty = dirty || i.dirty; });
+    })
+    this.dirty = dirty;
+    if (!this.dirty) { return }
+    this.canvas.clear()
+    this.canvas.ctx.drawImage(this.floor_canvas,0,0);
+    this.canvas.ctx.translate(-offset_x,-offset_y);
     uR.forEach(this.pieces,function(p){ p.draw() })
-    this.game.player.draw(this.canvas);
-    this.game.player.drawMoves(this.canvas);
+    this.game.player.drawMoves();
+    this.game.player.draw();
     uR.forEach(this.pieces,function(p){ p.draw() })
-    uR.forEach(this.pieces,function(p){ p.drawHealth() })
-    this.game.player.drawHealth(this.canvas);
-    this.canvas.ctx.translate(offset_x,offset_y)
+    uR.forEach(this.pieces,function(p){ p.drawHealth(); })
+    this.game.player.drawHealth();
+    this.canvas.ctx.translate(offset_x,offset_y);
   }
   createCanvas() {
     this.canvas = this.newCanvas({
       height: this.scale*this.H,
       width: this.scale*this.W,
       parent: document.getElementById("game")
+    });
+    this.floor_canvas = this.newCanvas({
+      height: this.scale*this.H,
+      width: this.scale*this.W,
     });
   }
   remove(piece) {
