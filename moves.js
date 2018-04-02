85aa1c377cfd80be73e5a64838a29ef4bcc0a17c
chriscauley <chris@lablackey.com>
2018-04-01 20:40:44 -0400

    skeleton can move around obstacles, to a small extent

--- moves.js
+++ moves.js
@@ -6,17 +6,23 @@ class Moves extends CanvasObject {
     if (!this.following) { return }
     var dx = this.following.x - this.x; //how far in each direction
     var dy = this.following.y - this.y;
+    var square =this.look();
     if (this.following.is_dead || Math.abs(dx) + Math.abs(dy) > this.sight*2) { this.following = undefined; return }
 
+    var square_x = this.look(Math.sign(dx),0);
+    var x_open = square_x && (!square_x.piece || square_x.piece.team != this.team);
+    var square_y = this.look(0,Math.sign(dy));
+    var y_open = square_y && (!square_y.piece || square_y.piece.team != this.team);
+    if (x_open && this.dx && Math.sign(dx) == this.dx) { return this.forward(); }
+    if (y_open && this.dy && Math.sign(dy) == this.dy) { return this.forward(); }
+
     var move = {};
-    if (this.dx && Math.sign(dx) == this.dx) { return this.forward(); }
-    if (this.dy && Math.sign(dy) == this.dy) { return this.forward(); }
-    if (Math.abs(dx) < Math.abs(dy)) {
-      if ( !this.dy ) { move = { turn: [0,Math.sign(dy)] } }
+    if (y_open && Math.abs(dx) < Math.abs(dy)) { // is x distance smaller than y distance? if so persue in y
+      if ( !this.dy ) { move = { turn: [0,Math.sign(dy)] } } // currently lookin in x so turn in y
       else if ( Math.sign(dy) != this.dy ) { move = this.flip(); } // facing away from enemy
-    } else {
+    } else if (x_open) { //target is closer in y (same as above, but flipped)
       if ( !this.dx ) { move = { turn: [Math.sign(dx),0] } }
-      else if ( Math.sign(dx) != this.dx ) { move = this.flip(); } // facing away from enemy
+      else if ( Math.sign(dx) != this.dx ) { move = this.flip(); }
     }
     move.chain = this.forward;
     return move;
