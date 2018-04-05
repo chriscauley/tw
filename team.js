0d723f2afe5c2138cb92ab42051124373526d99b
chriscauley <chris@lablackey.com>
2018-04-05 19:30:08 -0400

    add more than one piece with each team using game_cofig.base_units

--- team.js
+++ team.js
@@ -5,21 +5,25 @@ class Team extends uR.Object {
       game: uR.REQUIRED,
     })
     this.number = this.game.teams.length;
-    this.start = [(this.number == 1)?2:17,2];
+
+    this.start = [(this.number == 1)?2:(uR.tw.game_config.get("W")-2),2];
     this.dx = (this.number == 1)?1:-1;
     this.dy = 0;
+    this.pieces = [];
   }
   makeUnits() {
     // this should probably be it's own class
     var square = this.game.board.getSquare(this.start);
-    this.pieces = [
-      new Grave({
-        x: this.start[0],
-        y: this.start[1],
-        board: this.game.board,
-        team: this.number,
-      })
-    ]
+    for (var i=0;i<uR.tw.game_config.get("base_units");i++) {
+      this.pieces = [
+        new Grave({
+          x: this.start[0],
+          y: this.start[1],
+          board: this.game.board,
+          team: this.number,
+        })
+      ]
+    }
     return this.pieces;
   }
 }
