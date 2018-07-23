ead029385cdc24dfcb77ec5073c3fafd8a37ce72
chriscauley <chris@lablackey.com>
2018-07-23 00:16:40 +0000

    adding elephant

---
+++ src/piece/elephant.js
@@ -0,0 +1,23 @@
+class PygmyElephant extends tW.pieces.BasePiece {
+  constructor(opts={}) {
+    uR.defaults(opts,{wait_interval: 1});
+    super(opts);
+    this.setTasks(
+      tW.move.target(tW.move.buffSelf(tW.buffs.Charge),{instant: true}),
+      tW.move.findEnemy,
+      this.wait,
+      tW.move.follow,
+    )
+  }
+}
+
+class WarElephant extends tW.pieces.BasePiece {
+  constructor(opts={}) {
+    opts.health=4;
+    opts.wait_interval = 0;
+    super(opts);
+  }
+}
+
+tW.enemy_map.we = tW.pieces.WarElephant = WarElephant;
+tW.enemy_map.pe = tW.pieces.PygmyElephant = PygmyElephant;
