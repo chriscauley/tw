35fc0a534756c427ce00c638d00464b97ed6b7ee
chriscauley <chris@lablackey.com>
2018-07-28 16:17:29 +0000

    adding jambiya

--- src/item/weapon/base.js
+++ src/item/weapon/base.js
@@ -25,27 +25,30 @@ tW.weapon.BaseWeapon = class BaseWeapon extends tW.item.Item {
       var deltas = tW.look[this.geometry][[dx,dy]][r];
 
       const squares = this.piece.lookMany(deltas);
-      const square = _.find( //get the first matching square in this geometry
-        squares,
-        s=> s && s.piece && s.piece.team != this.piece.team
-      );
-      if (!square) { continue }
-      const action = {
-        damage: {
-          squares: this.splash?squares:[square],
-          count: this.damage,
-          dx: dx,
-          dy: dy,
-        }
-      }
-      // katana and scythe will dash one square if they are attacking 2 squares
-      // not sure how this should be handled at longer distances
-      if (this.dash && r>1) {
-        const d = Math.max(r-1,this.dash);
-        action.move = [Math.sign(dx)*d,Math.sign(dy)*d];
-      }
-      return action;
+      const action = this._getMoveForSquares(squares,r)
+      if (action) { return action }
     }
+  }
+  _getMoveForSquares(squares,range) {
+    const square = _.find( //get the first matching square in this geometry
+      squares,
+      s=> s && s.piece && s.piece.team != this.piece.team
+    );
+    if (!square) { return }
+    const action = {
+      damage: {
+        squares: this.splash?squares:[square],
+        count: this.damage,
+      }
+    }
+
+    // katana and scythe will dash one square if they are attacking 2 squares
+    // not sure how this should be handled at longer distances
+    if (this.dash && range>1) {
+      const d = Math.max(range-1,this.dash);
+      action.move = [Math.sign(dx)*d,Math.sign(dy)*d];
+    }
+    return action;
   }
 }
 
@@ -86,3 +89,26 @@ const _weapons = {
 }
 
 for (let key in _weapons) { defineWeapon(key,_weapons[key]) }
+
+tW.weapon.Jambiya = class Jambiya extends tW.weapon.BaseWeapon {
+  constructor(opts={}) {
+    opts.splash = true;
+    super(opts);
+  }
+  getMove(dx,dy) {
+    if (!dx && !dy) { return }
+    var action = super.getMove(dx,dy)
+    if (action) { console.log(action);return action } // damage square in front
+    const square = this.piece.look([dx,dy])
+    if (!square || square.piece) { return }
+    var damage_squares
+    if (dx) { // moving in x
+      damage_squares = square.lookMany([[0,1],[0,-1]])
+    } else if (dy) { // moving in y
+      damage_squares = square.lookMany([[1,0],[-1,0]])
+    }
+    action = this._getMoveForSquares(damage_squares)
+    if (action) { action.move = square }
+    return action
+  }
+}
