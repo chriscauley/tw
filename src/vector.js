6a592d81bc861d79de9dd686165da7b6f4555e0b
chriscauley <chris@lablackey.com>
2018-08-24 19:42:41 +0000

    adding vector operations to window.tV in order to not have to split dxdy so much

---
+++ src/vector.js
@@ -0,0 +1,11 @@
+// Some vector functions that will be used throughout timewalker
+window.tV = {
+  right: ([dx,dy]) => [-dy,dx],
+  left: ([dx,dy]) => [dy,-dx],
+  back: ([dx,dy]) => [-dx,-dy],
+  sign: ([dx,dy]) => [Math.sign(dx),Math.sign(dy)],
+  times: ([dx,dy],num) => [dx*num,dy*num],
+  add: ([dx,dy],[dx2,dy2]) => [dx+dx2,dy+dy2],
+  sum: (...dxdys) => dxdys.reduce(tV.add,[0,0]),
+  magnitude: ([dx,dy]) => Math.abs(dx || dy), // not tested for diagonals!
+}
