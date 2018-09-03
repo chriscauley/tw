0943b40d293bf9d2e33c6a9461918c90eb7d8d4d
chriscauley <chris@lablackey.com>
2018-09-03 22:33:26 +0000

    adding hit animations

---
+++ src/credits.js
@@ -0,0 +1,8 @@
+tW.credits = {
+  hit_animations: {
+    url: "https://opengameart.org/content/more-pixelated-attackhit-animations"
+    author: "Viktor Hahn",
+    contacted: false,
+    title: "More Pixelated Attack/Hit Animations",
+  }
+}
