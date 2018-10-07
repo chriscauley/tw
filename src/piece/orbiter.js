50310b123180be03707a8bacd4566e658995170c
chriscauley <chris@lablackey.com>
2018-10-07 19:37:38 +0000

    orbiter now shows action ready properly

--- src/piece/orbiter.js
+++ src/piece/orbiter.js
@@ -1,24 +1,25 @@
-
-
 class Orbiter extends tW.pieces.BasePiece {
   constructor(opts={}) {
     uR.defaults(opts,{
       _sprite: "moon",
-      radius: 2,
+      orbit_size: 2,
       turn_direction: "left",
       find: tW.move.findEnemy,
     })
     super(opts)
+    this.turn_wait = tW.move.wait(opts.orbit_size,{ name:'turn_wait',blocking: false })
     this.setTasks(
-      tW.move.wait(2,{ name:'turn_wait',blocking: false })
-        .then(
-          opts.find,
-          tW.move.follow,
-          tW.move.turn(opts.turn_direction),
-        ),
+      this.turn_wait.then(
+        opts.find,
+        tW.move.follow,
+        tW.move.turn(opts.turn_direction),
+      ),
       tW.move.forward,
-      tW.move.wait,
+      this.wait,
     )
+  }
+  isActionReady() {
+    return this.turn_wait.isReady();
   }
 }
 
