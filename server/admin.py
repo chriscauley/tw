f43e36b865f4e5ea931d9c7e2afaa9bba8f6d90c
chriscauley <chris@lablackey.com>
2019-02-21 20:26:05 +0000

    show sprites in admin

--- server/admin.py
+++ server/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from .models import Sheet
+from .models import Sheet, Sprite
 
 
 @admin.register(Sheet)
 class SheetAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Sprite)
+class SpriteAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "img"]
+
+    def img(self, obj):
+        img = "<img src='%s' width=64/>" % obj.data['dataURL']
+        return mark_safe(img)
