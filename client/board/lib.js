c7037f7ad665a3fc22657e57eca8256a63f85dff
chriscauley <chris@lablackey.com>
2019-04-12 03:41:19 +0000

    shuffle instead of randomly trying 100x for randomEmptyXY

--- client/board/lib.js
+++ client/board/lib.js
@@ -1,11 +1,11 @@
 import { canMoveOn } from '../piece/lib'
 
 export const randomEmptyXY = (board, xys) => {
-  for (let _i = 0; _i < 100; _i++) {
-    const xy = board.random.choice(xys)
-    if (canMoveOn({ board }, xy)) {
-      return xy
-    }
+  // #! TODO is cloning this and shuffling slow?
+  xys = board.random.shuffle([...xys])
+  const target_xy = xys.find(xy => canMoveOn({ board }, xy))
+  if (!target_xy) {
+    throw 'unable to find square!'
   }
-  throw 'unable to find square!'
+  return target_xy
 }
