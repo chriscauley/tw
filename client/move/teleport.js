e277db65baa3484debfcd7b97d2587c96f547620
chriscauley <chris@lablackey.com>
2019-05-02 23:32:25 +0000

    teleport now uses "three" geometry instead of circle

--- client/move/teleport.js
+++ client/move/teleport.js
@@ -1,22 +1,34 @@
 import geo from '../geo'
-import { randomEmptyXY } from '../lib'
+import { canMoveOn } from '../lib'
+import _ from 'lodash'
 import Random from 'ur-random'
 
-const _circle = geo.look._circle['1,0']
+const absmax = dxys => _.maxBy(dxys, geo.vector.magnitude)
+const target_dxys = _.range(9).map(range =>
+  _.flatten(geo.dxy.list.map(dxy => geo.look._three[dxy][range])),
+)
 
-export default range => (piece, move) => {
-  const target_xy = randomEmptyXY(
-    piece.board,
-    geo.look.lookMany(piece.xy, _circle[range]),
-  )
-  const [dx, dy] = geo.vector.subtract(piece.xy, target_xy)
-  const target_dxys = [[dx, 0], [0, dy]].filter(dxy => !geo.vector.isZero(dxy))
-  const dxy = geo.vector.sign(Random.fp.choice(piece, target_dxys))
-  return {
-    ...move,
-    xy: target_xy,
-    dxy,
-    done: true,
-    end: true, // takes all turns
+export default max_range => {
+  return (piece, move) => {
+    let range = Math.abs(max_range)
+    while (range) {
+      const target_dxy = Random.fp
+        .shuffle(piece, [...target_dxys[range]])
+        .find(dxy => canMoveOn(piece, geo.vector.add(dxy, piece.xy)))
+      if (!target_dxy) {
+        range--
+        continue
+      }
+      const target_xy = geo.vector.add(target_dxy, piece.xy)
+      const [dx, dy] = geo.vector.subtract(piece.xy, target_xy)
+      const dxy = geo.vector.sign(absmax([[dx, 0], [0, dy]]))
+      return {
+        ...move,
+        xy: target_xy,
+        dxy,
+        done: true,
+        end: true, // takes all turns
+      }
+    }
   }
 }
