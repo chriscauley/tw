45e7336ba9c43929ec2a12cd38429393bde10970
chriscauley <chris@lablackey.com>
2019-05-30 02:18:26 +0000

    shoot now shows fireball on paint

--- client/move/shoot.js
+++ client/move/shoot.js
@@ -1,13 +1,22 @@
 import after from './after'
 import geo from '../geo'
 
-export default _type => (piece, move, dxy = piece.dxy) => {
-  const xy = geo.vector.add(piece.xy, dxy)
-  if (piece.board.canAddFire(xy)) {
-    move = after(move, () => {
-      piece.board.addFire(1, xy, dxy)
-    })
-    move.done = true
+export default _type => {
+  const action = (piece, move, dxy = piece.dxy) => {
+    const xy = geo.vector.add(piece.xy, dxy)
+    if (piece.board.canAddFire(xy)) {
+      move = after(move, () => {
+        piece.board.addFire(1, xy, dxy)
+      })
+      move.done = true
+    }
+    return move
   }
-  return move
+  action.paint = (piece, _move) => [
+    {
+      xy: piece.xy,
+      sprite: 'fireball dxy-' + piece.dxy.join(''),
+    },
+  ]
+  return action
 }
